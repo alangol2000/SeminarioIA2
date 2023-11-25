41096d1a79002d8fec89eb545d4b3c7881495b47
Alan <61305178+alangol2000@users.noreply.github.com>
2023-11-24 22:40:12 -0600

    Add files via upload

---
+++ Proyecto Final/Máquinas de Soporte Vectorial.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+# Descargar el conjunto de datos Zoo y cargarlo en un DataFrame
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/zoo/zoo.data"
+column_names = ["Animal_Name", "Hair", "Feathers", "Eggs", "Milk", "Airborne", "Aquatic", "Predator", "Toothed",
+"Backbone", "Breathes", "Venomous", "Fins", "Legs", "Tail", "Domestic", "Catsize", "Type"]
+data = pd.read_csv(url, header=None, names=column_names)
+
+# Eliminar la columna de nombres de animales
+data = data.drop(columns=["Animal_Name"])
+
+# Dividir el conjunto de datos en características (X) y etiquetas (y)
+X = data.drop(columns=["Type"])
+y = data["Type"]
+
+# Dividir el conjunto de datos en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Inicializar y entrenar el modelo de Máquinas de Soporte Vectorial
+model = SVC()
+model.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = model.predict(X_test)
+
+# Calcular las métricas de evaluación
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average="weighted")
+recall = recall_score(y_test, y_pred, average="weighted")
+f1 = f1_score(y_test, y_pred, average="weighted")
+
+# Calcular Specificity
+conf_matrix = confusion_matrix(y_test, y_pred)
+true_negatives = conf_matrix[0, 0]
+false_positives = conf_matrix[0, 1]
+specificity = true_negatives / (true_negatives + false_positives)
+
+# Imprimir las métricas
+print(f"Accuracy: {accuracy:.2f}")
+print(f"Precision: {precision:.2f}")
+print(f"Sensitivity: {recall:.2f}")
+print(f"Specificity: {specificity:.2f}")
+print(f"F1 Score: {f1:.2f}")
